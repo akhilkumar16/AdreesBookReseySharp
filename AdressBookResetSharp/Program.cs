31d9e4d7bdeaec5de159af8b3b5d4238d7cdfb3a
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-06 01:57:45 +0530

    [Akhil] Address Book

---
+++ AdressBookResetSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdressBookResetSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ResetSharp program of addressbook");
+        }
+    }
+}
